d990b2f1e94de3b48161700ae36d719dec18691a
knies <knies@10.0.1.47>
2017-05-04 22:04:11 -0400

    Finished

--- PS6_JavaFx/src/main/java/ch/makery/address/view/PersonOverviewController.java
+++ PS6_JavaFx/src/main/java/ch/makery/address/view/PersonOverviewController.java
@@ -113,8 +113,7 @@ public class PersonOverviewController {
         	//		Figure out the value of perID
         	
         	UUID perID = UUID.fromString("1234");        	
-        	//TODO: Call Delete Person in PersonDAL 
-        	
+        	PersonDAL.deletePerson(perID);
         	
             personTable.getItems().remove(selectedIndex);
             
@@ -140,8 +139,8 @@ public class PersonOverviewController {
         Person tempPerson = new Person();
         boolean okClicked = mainApp.showPersonEditDialog(tempPerson);
         if (okClicked) {
-        	//PS6 - Calling the addPerson method
-        	//TODO: Call Add Person in PersonDAL        	
+        	//PS6 - Calling the addPerson method  
+        	PersonDAL.addPerson(tempPerson);
             mainApp.getPersonData().add(tempPerson);
         }
     }
@@ -158,7 +157,7 @@ public class PersonOverviewController {
             if (okClicked) {
             	
             	//PS6 - Calling the updatePerson method
-            	//TODO: Call Update Person in PersonDAL 
+            	PersonDAL.updatePerson(selectedPerson);
                 showPersonDetails(selectedPerson);
             }
 
