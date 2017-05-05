45db589e4829a16fcda4d86c43abd663fe31708e
knies <knies@10.0.1.47>
2017-05-04 22:03:12 -0400

    Finished

--- PS6_DAL/src/test/java/base/Person_Test.java
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -2,6 +2,7 @@ package base;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import org.junit.After;
@@ -13,13 +14,28 @@ import org.junit.Test;
 import domain.PersonDomainModel;
 
 public class Person_Test {
+	
+	static PersonDomainModel person = new PersonDomainModel();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
+		person.setFirstName("Kirstie");
+		person.setLastName("Niessen");
+		person.setCity("Kennett Square PA");
+		person.setBirthday(LocalDate.now());
+		person.setPostalCode(19348);
+		person.setStreet("136 West Thomas Court");
+		PersonDAL.addPerson(person);
+		PersonDomainModel person_2 = PersonDAL.getPerson(person.getPersonID());
+		
+		assertNotNull(person_2);
+	}
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		
 	}
 
 	@Before
@@ -28,21 +44,36 @@ public class Person_Test {
 
 	@After
 	public void tearDown() throws Exception {
+		
+		PersonDAL.deletePerson(person.getPersonID());
 	}
 
 	@Test
 	public void AddPerson() {
-		PersonDomainModel per1 = new PersonDomainModel();
-		per1.setBirthday(new Date(0));
-		per1.setCity("Townsend");
-		per1.setFirstName("Bert");
-		per1.setLastName("Gibbons");
-		per1.setPostalCode(19734);
-		per1.setStreet("214 Labrador Lane");
+		PersonDAL.addPerson(person);
+		PersonDomainModel person_2 = PersonDAL.getPerson(person.getPersonID());
 		
-		PersonDAL.addPerson(per1);
+		assertNotNull(person_2);
+		
+	}
+	
+	@Test
+	public void UpdateDelete(){
+		
+		PersonDomainModel person_2 = PersonDAL.getPerson(person.getPersonID());
+		assertEquals(person.getPersonID(), person_2.getPersonID());
+		person_2.setLastName("Marsh");
+		PersonDAL.updatePerson(person_2);
+		PersonDomainModel person_3 = PersonDAL.getPerson(person_2.getPersonID());
+		assertEquals(person_2.getLastName(), person_3.getLastName());
+		assertNotEquals(person.getLastName(), person_3.getLastName());	
+		PersonDAL.deletePerson(person.getPersonID());
+		PersonDomainModel person_4 = PersonDAL.getPerson(person.getPersonID());
+		assertNull(person_4);
 		
 		
 	}
 
-}
+		
+	}
+
